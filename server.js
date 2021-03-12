58de0322d016c31ecc22d2cfe17c12c6880d0fd4
Idzel Jaimes <irjaimes@outlook.com>
2021-03-12 15:37:34 -0800

    tested server connection

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+//connection test (overrides all others, therefore must be the last one)
+// app.get('/', (req, res) => {
+//     res.json({
+//       message: 'Hello World'
+//     });
+//   });
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
